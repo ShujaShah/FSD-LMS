c3e315f1ec8272859ff9512df1e79147dba7a39a
Shuja Shah <shuja.ashraf11@gmail.com>
2024-12-20 08:52:42 +0530

    implemented CRUD for Courses

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,9 +1,22 @@
 import express from 'express';
-import { getCourse, listCourse } from '../controllers/courseController';
+import multer from 'multer';
+
+import {
+  createCourse,
+  deleteCourse,
+  getCourse,
+  listCourse,
+  updateCourse,
+} from '../controllers/courseController';
+import { requireAuth } from '@clerk/express';
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', listCourse);
-router.get('/:courseId', getCourse);
+router.post('/', requireAuth(), createCourse);
 
+router.get('/:courseId', getCourse);
+router.put('/:courseId', requireAuth(), upload.single('image'), updateCourse);
+router.delete('/:courseId', requireAuth(), deleteCourse);
 export default router;
