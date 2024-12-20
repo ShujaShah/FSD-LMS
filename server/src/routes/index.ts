8859d0fb661b614a12f79117e5049e252ad213ab
Shuja Shah <shuja.ashraf11@gmail.com>
2024-12-20 12:11:57 +0530

    course view

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,11 +4,13 @@ import courseRoutes from './courseRoutes';
 import userClerkRoutes from './userClerkRoutes';
 import transactionRoutes from './transactionRoutes';
 import { requireAuth } from '@clerk/express';
+import userCourseProgressRoutes from './userCourseProgressRoutes';
 
 const router = express.Router();
 
 router.use('/courses', courseRoutes);
 router.use('/users/clerk', userClerkRoutes);
 router.use('/transactions', requireAuth(), transactionRoutes);
+router.use('users/course-progress', requireAuth(), userCourseProgressRoutes);
 
 export default router;
